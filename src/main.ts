988fb5e554a41a2429383b34d739df0f24eae876
Elim Florvil <elim.florvil@icloud.com>
2023-10-03 09:41:27 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import "tailwindcss/tailwind.css";
+import "./assets/main.css";
 
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-const app = createApp(App)
+import App from "./App.vue";
+import router from "./router";
 
-app.use(createPinia())
-app.use(router)
+const app = createApp(App);
 
-app.mount('#app')
+app.use(createPinia());
+app.use(router);
+
+app.mount("#app");
