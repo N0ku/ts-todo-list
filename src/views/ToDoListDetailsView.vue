30d89b8a8d34d545b5518bdbeec2aff626b7a971
Eddy Muzaffar <eddy.muzaffar@outlook.com>
2023-10-03 12:06:16 +0200

    add modal form

--- src/views/ToDoListDetailsView.vue
+++ src/views/ToDoListDetailsView.vue
@@ -1,10 +1,12 @@
 
 <script setup lang="ts">
 import ListItem from "@/components/ListItem.vue";
+import ModalFormAdd from "@/components/modal/ModalFormAdd.vue";
 </script>
 
 <template>
   <div>
     <ListItem />
+    <ModalFormAdd></ModalFormAdd>
   </div>
 </template>
